530d153fb53a10e18cbbd3e14ee68323e418eb5d
Parag Joshi <paragjoshi0820@gmail.com>
2026-04-09 11:52:55 +0530

    first commit

---
+++ src/services/endpoints.js
@@ -0,0 +1,9 @@
+// all API URLs
+//  for examples
+// export const ENDPOINTS = {
+//   LOGIN: "/auth/login",
+//   REGISTER: "/auth/register",
+//   GET_PROFILE: "/user/profile",
+//   UPDATE_PROFILE: "/user/update",
+//   GET_BANNERS: "/banners",
+// };
---
+++ src/utils/storage.js
@@ -0,0 +1,1 @@
+// localStorage / AsyncStorage logic
