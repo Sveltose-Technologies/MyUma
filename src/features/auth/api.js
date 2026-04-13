86fcb54b7bf0e2c88ff050c3b3473522ad8bc4d5
Parag Joshi <paragjoshi0820@gmail.com>
2026-04-13 16:53:10 +0530

    "forget-password"

--- src/features/auth/api.js
+++ src/features/auth/api.js
@@ -16,10 +16,21 @@ export const registerAPI = async (userData) => {
 
 // ✅ VERIFY OTP
 export const verifyOtpAPI = async (data) => {
+  console.log("data otp", data);
+
   const response = await API.post("/auth/verify-otp", data);
+  console.log("OTP verification response:", response);
   return response.data;
 };
 export const forgotPasswordAPI = async (data) => {
   const response = await API.post("/auth/forgot-password", data);
+  return response.data;
+};
+export const resetPasswordAPI = async (data) => {
+  console.log("chnage password", data);
+
+  const response = await API.put("/auth/reset-password", data);
+  console.log("api response", response);
+
   return response.data;
 };
